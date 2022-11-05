6f9b0b52d78ed762b315e48510ca3247ea9017e6
Brian Yoder <brianyoder@gmail.com>
2022-11-04 21:56:26 -0700

    add branching

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,38 @@
+
+let python = "Python"
+let javascript = "Javascript"
+let ruby = "Ruby"
+let assembly = "Assembly Language"
+let swift = "Swift"
+
+
 window.addEventListener("load", function() {
-  let form = document.querySelector("form");
-  let button = document.getElementById("submit");
+  const form = document.querySelector("form");
+  const button = document.getElementById("submit");
 
   form.addEventListener("submit", function(event) {
+    event.preventDefault();
     const foodAnswer = document.querySelector("input[name='food']:checked").value;
+
+    if (foodAnswer === "hotdog") {
+      document.querySelector("span#language") = python;
+    } else if (foodAnswer === "hamburger") {
+      document.querySelector("span#language") = javascript;
+    } else if (foodAnswer === "kimchi") {
+      document.querySelector("span#language") = ruby;
+    } else if (foodAnswer === "celeryjuice") {
+      document.querySelector("span#language") = assembly;
+    } else if (foodAnswer === "buffalowings") {
+      document.querySelector("span#language") = swift
+    }
+    
+  
+  form.addEventListener("submit", function() {
+      button.removeAttribute("class");
+      form.setAttribute("class", "hidden");
+  
+  })})})
+
+
+
+
